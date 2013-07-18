23590c3c3c8a4fdb0bb116704883e71f78ef75ba
pwned <pwned@pwned.(none)>
2013-07-18 04:07:15 +0200

    Add the django admin part

--- bda/iw/models.py
+++ bda/iw/models.py
@@ -5,11 +5,3 @@ class msg(models.Model):
     title = models.TextField()
     msg = models.TextField()
     date = models.DateTimeField()
-
-class member(models.Model):
-    id = models.AutoField(primary_key=True)
-    pseudo = models.TextField()
-    email = models.EmailField()
-    pwd = models.TextField()
-    co = models.BooleanField()
-    img = models.TextField()
--- bda/urls.py
+++ bda/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
                        url('^$', 'bda.iw.views.Index'),
                        url(r'^index/$', 'bda.iw.views.Index'),
-                       url(r'^index2/$', 'bda.iw.views.Index2'),
+                       url(r'^admin/', include(admin.site.urls)),
                        url(r'^co/$', 'bda.iw.views.Co'),
                        url(r'^register/$', 'bda.iw.views.Register'),
                        url(r'^blog/$', 'bda.iw.views.Blog'),
